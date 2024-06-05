2af47058837ea327c34f80be3e65ef418dd894d7
G5DPm5 <sAZIUC9Vy0O@protonmail.com>
2024-06-05 11:10:40 +0800

    Fix: Nodes output

--- Scripts/SubStore/NodeRenamer.js
+++ Scripts/SubStore/NodeRenamer.js
@@ -34,8 +34,8 @@ function operator(proxies = [], targetPlatform, context) {
     if (!result.match(number_rules[0])) {
       result = result.replace(number_rules[1], "0$1 ");
     }
-
-    proxy.name = name + " " + result.replace(/\s\s/, " ");
+    proxy.name =
+      name + " " + result.replaceAll("-", " ").replaceAll(/\s+/g, " ");
   });
 
   return proxies;
@@ -49,8 +49,8 @@ const country_name = {
 };
 // Number 1->01 11->11
 const number_rules = [
-  new RegExp("(\\d{2})-?(?!X|x)"),
-  new RegExp("(\\d)-?(?!X|x)"),
+  new RegExp("(\\d{2})(?!X|x)"),
+  new RegExp("(\\d)(?!X|x)"),
 ];
 
 const node_coefficient_rules = [
