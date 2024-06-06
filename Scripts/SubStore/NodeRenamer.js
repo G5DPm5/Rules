8e53597f561f67b80bbe9511f90f8052221176bf
G5DPm5 <sAZIUC9Vy0O@protonmail.com>
2024-06-06 11:55:36 +0800

    Add: delete invalid node

--- Scripts/SubStore/NodeRenamer.js
+++ Scripts/SubStore/NodeRenamer.js
@@ -1,4 +1,13 @@
 function operator(proxies = [], targetPlatform, context) {
+  const reg =
+    /邀请|返利|循环|官网|客服|网站|网址|获取|订阅|流量|到期|机场|下次|版本|官址|备用|到期|过期|已用|联系|邮箱|工单|群|贩卖|通知|倒卖|防止|国内|🎮|GAME|USE|USED|TOTAL|EXPIRE|EMAIL|Panel/;
+  proxies.filter((proxy) => {
+    if (!reg.test(proxy.name)) {
+      return true;
+    }
+    return false;
+  });
+
   const name = $arguments.name;
   proxies.forEach((proxy = { name }) => {
     let result = proxy.name;
